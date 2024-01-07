728c760672957bc9968c13ce734192d4a0790d78
Aswin Haridas <aswinvharidas@gmail.com>
2024-01-07 23:59:39 +0530

    Sample Unit Test added.

--- src/test/kotlin/com/scorp/easydone/EasydoneApplicationTests.kt
+++ src/test/kotlin/com/scorp/easydone/EasydoneApplicationTests.kt
@@ -1,13 +1,30 @@
 package com.scorp.easydone
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import kotlin.math.exp
 
-@SpringBootTest
 class EasydoneApplicationTests {
 
+	val underTest = Calculator()
 	@Test
-	fun contextLoads() {
+	fun itShouldAddNumbers() {
+//		given
+		val numberOne = 20
+		val numberTwo = 23
+		val expected = 43
+//		when
+		val result = underTest.add(numberOne, numberTwo)
+
+//		then
+		assertThat(result).isEqualTo(expected)
+	}
+
+	class Calculator{
+		fun add(a:Int,b:Int):Int{
+			return a+b
+		}
 	}
 
 }
